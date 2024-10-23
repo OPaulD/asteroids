db37fa5a4f16bf2ca47d4bcddf934d1078c873a0
OPaulD <omota.paul@gmail.com>
2024-10-24 00:40:47 +0100

    add refresh rate

--- main.py
+++ main.py
@@ -13,13 +13,18 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-                
+
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        clock_output = clock.tick(60)
+        dt = clock_output / 1000
 
 
 
